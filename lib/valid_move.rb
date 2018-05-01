70361e04dc741d95e5c901dc783eea36b04b2054
Benjamin Krigel <benjamin.krigel@powerhrg.com>
2018-05-01 16:34:41 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,5 @@
 def valid_move?(board, index)
-    if index.between?(0,8) &&  position_taken?(board,index)
+    if index.between?(0,8) && !position_taken?(board,index)
       true
     end
 end
@@ -8,7 +8,7 @@ end
 def position_taken?(board, index)
   if board[index] == " " || board[index] == ""
     return false
-  else 
+  else
     return true
   end
 end
